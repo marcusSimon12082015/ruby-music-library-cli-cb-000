eaeb7790079c25756c49b9efd72b6c3bebf75fa8
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-07 19:50:05 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -24,7 +24,7 @@ class MusicLibraryController
     sorted_list.each_with_index{|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
   def list_artists
-    sorted_list = @music_importer.files.sort!{|a,b| a.artist.name <=> b.artist.name}
-    sorted_list.each_with_index{|song, index| puts "#{index+1}. #{song.artist.name}"}.uniq
+    sorted_list = @music_importer.files.sort!{|a,b| a.artist.name <=> b.artist.name}.uniq
+    sorted_list.each_with_index{|song, index| puts "#{index+1}. #{song.artist.name}"}
   end
 end
